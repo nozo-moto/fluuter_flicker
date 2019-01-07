454da175d1bafb3117394484e5f5d39dbfae9cd2
nozomi0966 <nozomotoitech@gmail.com>
2019-01-07 23:41:24 +0900

    write photoset model

---
+++ lib/model/photo.dart
@@ -0,0 +1,36 @@
+class Photo {
+  const Photo(
+    this.id,
+    this.secret,
+    this.server,
+    this.farm,
+    this.title,
+    this.isprimary,
+    this.ispublic,
+    this.isfriend,
+    this.isfamily
+  );
+  final String id;
+  final String secret;
+  final String server;
+  final int farm;
+  final String title;
+  final String isprimary;
+  final String ispublic;
+  final String isfriend;
+  final String isfamily;
+
+  static fromJson(json) {
+    return new Photo(
+      json['id'],
+      json['secret'],
+      json['server'],
+      json['farm'],
+      json['title'],
+      json['isprimary'],
+      json['ispublic'],
+      json['isfriend'],
+      json['isfamily']
+    );
+  }
+}
---
+++ lib/model/photosets.dart
@@ -0,0 +1,62 @@
+import 'package:fluuter_flicker/model/photo.dart';
+
+class Photoset {
+  const Photoset(
+    this.id,
+    this.primary,
+    this.owner,
+    this.ownername,
+    this.photo,
+    this.page,
+    this.per_page,
+    this.perpage,
+    this.pages,
+    this.title,
+    this.total
+  );
+
+  final String id;
+  final String primary;
+  final String owner;
+  final String ownername;
+  final Photo photo;
+  final int page;
+  final String per_page;
+  final String perpage;
+  final int pages;
+  final String title ;
+  final int total;
+
+  static fromJson(json) {
+    return new Photoset(
+      json['id'],
+      json['primary'],
+      json['owner'],
+      json['ownername'],
+      json['photo'],
+      json['page'],
+      json['per_page'],
+      json['perpage'],
+      Photo.fromJson(json['photo']),
+      json['title'],
+      json['total']
+    );
+  }
+}
+
+class Photosets_getphotos {
+  const Photosets_getphotos(
+    this.photoset,
+    this.stat
+  );
+
+  final Photoset photoset;
+  final String stat;
+
+  static fromJson(json) {
+    return new Photosets_getphotos(
+      json['photoset'], 
+      json['stat']
+    );
+  }
+}
