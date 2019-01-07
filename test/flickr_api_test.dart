f14b5430cb46d1710f5db3884db1c043fcc1b92e
nozomi0966 <nozomotoitech@gmail.com>
2019-01-08 01:33:00 +0900

    add dotenv and remove apikey

--- test/flickr_api_test.dart
+++ test/flickr_api_test.dart
@@ -1,11 +1,13 @@
 import 'package:fluuter_flicker/api/flickr_impl.dart';
 import "package:test/test.dart";
+import 'package:dotenv/dotenv.dart' show load, clean, isEveryDefined, env;
 
 void main() {
+  load();
+  var APIkey = env['FLICKR_APIKEY'];
   const photosetID = "72157690900917721";
-  const APIkey = "a7ccd16085e06cc6e8dd0e56dfba65ad";
+  
   test('flickr api test', () async {
     var result = await FlickrAPIImpl().getPhotoset(photosetID, APIkey);
-    print(result);
   });
 }
